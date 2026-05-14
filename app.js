1e00a277715c89900fc712f95469dc5f5f1349e4
samuelp567 <samuel.prince@stu.barren.kyschools.us>
2026-05-14 10:45:20 -0500

    Started Project, added collection and starter files, Changed coloring

---
+++ app.js
@@ -0,0 +1,6 @@
+const collection = [
+    {item:"plastic Scrap" ,category: "Resources", rarity:"Common" , notes:"A bag of plastic odds and ends."},
+    {item:"Duct Tape",category:"Resources",rarity:"common",notes:"You know exactly what this is."},
+    {item:"Sledgehammer",category:"Weapon",rarity:"Rare",notes:"An exceptionally heavy hammer for pounding things into the ground."},
+    {item:"Antique Shotgun",category:"Weapon",rarity:"Unique",notes:"GATE Immurement Registry: IS-0099"}
+]
